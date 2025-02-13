6524b8aee12ed973d274f5531bdec017ac2d17a2
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2025-02-13 10:13:21 +0500

    added user active functionality

--- chatmessanger/app/components/sidebar/DesktopSidebar.tsx
+++ chatmessanger/app/components/sidebar/DesktopSidebar.tsx
@@ -2,9 +2,17 @@
 import useRoutes from "@/hooks/useRoutes"
 import { useState } from "react"
 import DesktopItem from "./DesktopItem"
-const DestopSideBar = () => {
+import {User} from "@prisma/client"
+import Avatar from "../Avatar"
+interface DesktopSidebarProps{
+    currentUser: User
+}
+
+const DestopSideBar : React.FC<DesktopSidebarProps> = ({currentUser}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
+
+    console.log({currentUser})
     return(
         <div className="
            hidden
@@ -51,7 +59,26 @@ const DestopSideBar = () => {
             ))}
           </ul>
         </nav>
-        
+        <nav
+          className="
+            mt-4
+            flex
+            flex-col
+            justify-between
+            items-center
+          "
+        >
+          <div
+            onClick={() => setIsOpen(true)}
+            className="
+              cursor-pointer
+              hover:opacity-75
+              transition
+            "
+          >
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
         </div>
     );
 }
--- chatmessanger/app/components/sidebar/Sidebar.tsx
+++ chatmessanger/app/components/sidebar/Sidebar.tsx
@@ -1,11 +1,13 @@
 import DesktopSidebar from "@/components/sidebar/DesktopSidebar";
 import MobileFooter from "@/components/sidebar/MobileFooter"
+import getCurrentUser from "@/actions/getCurrentUser";
 async function Sidebar({ children }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
   return (
     <div className="h-full">
-      <DesktopSidebar/>
+      <DesktopSidebar currentUser={currentUser!}/>
       <MobileFooter/>
       <main className="lg:pl-20 h-full">
         {children}
